b90b310b3722b6cbb17b0aedfd16ee1b924cf0f3
AnastasiaKhoruzhenko <aakhoruzhenko@edu.hse.ru>
2020-02-19 21:49:34 +0300

    swipe up to add master in admin area

--- app/src/main/java/com/coursework/barbershopapp/Admin/ui/masters/MastersAdminFragment.java
+++ app/src/main/java/com/coursework/barbershopapp/Admin/ui/masters/MastersAdminFragment.java
@@ -16,19 +16,39 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.coursework.barbershopapp.Admin.AddMasterFragment;
+import com.coursework.barbershopapp.Admin.AddMasterViewModel;
 import com.coursework.barbershopapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MastersAdminFragment extends Fragment {
 
     private MastersAdminViewModel mViewModel;
     private Button btn_add;
+    private FloatingActionButton floating_add;
     private ArrayList<String> mNames = new ArrayList<>();
     private ArrayList<String> mImageUrls = new ArrayList<>();
+
+    private TextInputLayout tilSurname, tilName, tilPhone, tilEmail;
+    private TextInputEditText tit_surname, tit_name, tit_phone, tit_email;
+    private Button create_master;
+
+    private FirebaseAuth mAuth;
 
     public static MastersAdminFragment newInstance() {
         return new MastersAdminFragment();
@@ -43,22 +63,127 @@ public class MastersAdminFragment extends Fragment {
 
         View root = inflater.inflate(R.layout.masters_admin_fragment, container, false);
 
+        tilName = root.findViewById(R.id.til_name);
+        tilSurname = root.findViewById(R.id.til_surname);
+        tilPhone = root.findViewById(R.id.til_phone);
+        tilEmail = root.findViewById(R.id.til_email);
+        tit_name = root.findViewById(R.id.tit_name);
+        tit_surname = root.findViewById(R.id.tit_surname);
+        tit_phone = root.findViewById(R.id.tit_phone);
+        tit_email = root.findViewById(R.id.tit_email);
+
+        create_master = root.findViewById(R.id.btn_create_master);
+
+        create_master.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = tit_name.getText().toString();
+                String surname = tit_surname.getText().toString();
+                String phone = tit_phone.getText().toString();
+                String email = tit_email.getText().toString();
+                //Toast.makeText(getContext(), name, Toast.LENGTH_SHORT).show();
+                checkInfo(name, surname, phone, email);
+            }
+        });
+
 
         RecyclerView recyclerView = root.findViewById(R.id.recview_masters);
         RecyclerViewAdapter adapter = new RecyclerViewAdapter(getContext(), mNames, mImageUrls);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
 
-
-        btn_add = root.findViewById(R.id.btn_add_master);
-        btn_add.setOnClickListener(new View.OnClickListener() {
+        floating_add = root.findViewById(R.id.floating_fab_add);
+        View llBottomSheet = root.findViewById(R.id.bottom_sheet);
+        BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(llBottomSheet);
+        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(getContext(), "Im here", Toast.LENGTH_SHORT).show();
-                swapFragment();
+            public void onStateChanged(@NonNull View view, int i) {
+
+            }
+
+            @Override
+            public void onSlide(@NonNull View view, float v) {
+                floating_add.animate()
+                        .scaleX(1-v).scaleY(1-v)
+                        .setDuration(0)
+                        .start();
             }
         });
+
+
+//        // for bottom view
+//        LinearLayout llBottomSheet = root.findViewById(R.id.bottom_sheet);
+//
+//        BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(llBottomSheet);
+//
+//        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+//        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+//        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+//
+//        //bottomSheetBehavior.setPeekHeight();
+//
+//        bottomSheetBehavior.setHideable(false);
+//
+//        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+//            @Override
+//            public void onStateChanged(@NonNull View view, int i) {
+//
+//            }
+//
+//            @Override
+//            public void onSlide(@NonNull View view, float v) {
+//
+//            }
+//        });
+
+
+
         return root;
+    }
+
+    private void checkInfo(String name, String surname, String phone, String email) {
+        if(name.isEmpty() || surname.isEmpty() || email.isEmpty() || phone.isEmpty())
+        {
+            Toast.makeText(getContext(), "Add all required information", Toast.LENGTH_SHORT).show();
+        }else{
+            registerNewMasterWithDefaultPassword(email, name, surname, phone);
+        }
+    }
+
+    private void registerNewMasterWithDefaultPassword(final String email, final String name, final String surname, final String phone) {
+        mAuth = FirebaseAuth.getInstance();
+
+        // дефолтный пароль -> сделать для изменения в профиле админа
+        String defaultPassword = "barbershop";
+        mAuth.createUserWithEmailAndPassword(email, defaultPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    showMessage("Аккаунт успешно создан");
+                    updateInfo(name, surname, email, phone);
+                }
+                else
+                {
+                    showMessage("Пользователь с такой почтой уже существует");
+                }
+            }
+        });
+    }
+
+    private void updateInfo(String name, String surname, String email, String phone) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        Map<String, Object> master = new HashMap<>();
+        master.put("Name", name);
+        master.put("Surname", surname);
+        master.put("Phone", phone);
+        master.put("Email", email);
+        master.put("DefaultPass", true);
+        db.collection("Masters").document(email).set(master);
+
+    }
+
+    private void showMessage(String s) {
+        Toast.makeText(getContext(), s, Toast.LENGTH_SHORT).show();
     }
 
     private void swapFragment() {
